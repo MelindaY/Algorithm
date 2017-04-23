e4d513425eda0391fe3782837d400d334fa8ede2
MelindaY <yavejing@gmail.com>
2017-04-23 19:17:13 +0800

    rebuild code sort by DS

--- Algorithm/BinCount.java
+++ Algorithm/BinCount.java
@@ -1,4 +1,5 @@
 package Algorithm;
+
 /*
  * 题目要求：     统计某个数二进制1的个数。
  * ************************************************************************
@@ -32,36 +33,34 @@ package Algorithm;
  *          
  * */
 public class BinCount {
-	 public static int NumberOf1(int n) {
- 		 /* My WAY:*/
- 		  int count=0;
- 		 int re=0;
- 		 if(n>>31==-1)
- 		 {
- 			 //n=~n;
- 			for(int i=0;i<32;i++){
- 				re=n>>>i;
- 			    count+=re&1;
- 			}			 
- 			 return count;			 
- 		 }
-		 for(int i=0;(1<<i)<=n;i++)//2^i<n
-		 {
-			 count+=(n>>i)&1;
-			 
-		 }
-		 return count;
-		/* int count = 0; 
-		 while(n!= 0){ 
-			 count++; 
-			 n = n & (n - 1);
-			 } 
-		 return count;*/
-	    }
-	 public static void main(String[] args){
-		 System.out.println(Integer.toBinaryString(-6));//可以输出二进制
-		 System.out.println(NumberOf1(-6));
+	public static int NumberOf1(int n) {
+		/* My WAY: */
+		int count = 0;
+		int re = 0;
+		if (n >> 31 == -1) {
+			// n=~n;
+			for (int i = 0; i < 32; i++) {
+				re = n >>> i;
+				count += re & 1;
+			}
+			return count;
+		}
+		for (int i = 0; (1 << i) <= n; i++)// 2^i<n
+		{
+			count += (n >> i) & 1;
 
-	 }
+		}
+		return count;
+		/*
+		 * int count = 0; while(n!= 0){ count++; n = n & (n - 1); } return
+		 * count;
+		 */
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Integer.toBinaryString(-6));// 可以输出二进制
+		System.out.println(NumberOf1(-6));
+
+	}
 
 }
